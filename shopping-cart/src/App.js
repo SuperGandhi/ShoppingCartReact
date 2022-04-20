83fb7c5fbafa85246cb479b20e6f6968a946c33a
SuperGandhi <patrickborjabueno@hotmail.com>
2022-04-19 23:10:24 -0500

    finishing project

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -10,16 +10,15 @@ class App extends Component {
       {name: 'Arbejas', price: 2500, img:'/products/arbejas.jpg'},
       {name: 'Lechuga', price: 500, img:'/products/lechuga.jpg'},
     ],
-    carro:[
-      // {name: 'Tomate', price: 1500, img:'/products/tomate.jpg', cantidad:1},
-    ],
+    carro:[],
+    esCarroVisible: false,
   }
   
   agregarAlCarro = (producto) =>{
     const {carro} = this.state
     if(carro.find (x => x.name === producto.name)){
       const newCarro = carro.map(x=> x.name ===producto.name
-       ? ({
+      ? ({
         //operador ternario
         ...x,
         cantidad: x.cantidad + 1
@@ -36,11 +35,23 @@ class App extends Component {
     })
   }
 
+  mostrarCarro = () =>{
+    if(!this.state.carro.length){
+      return 
+    }
+    this.setState({esCarroVisible: !this.state.esCarroVisible})
+  }
+
   render (){
+    const {esCarroVisible} = this.state;
     console.log(this.state.carro)
     return (
       <div>
-        <Navbar/>
+        <Navbar 
+        carro={this.state.carro} 
+        esCarroVisible={esCarroVisible} 
+        mostrarCarro={this.mostrarCarro}
+        />
         <Layout>
         <Title/>
         <Products
